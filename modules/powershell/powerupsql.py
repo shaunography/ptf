88775e1255e7400b53fb2e718c8869521481ca1b
Shaun Webber <shaun.webber@sec-1.com>
2018-11-21 15:45:50 +0000

    added PowerUpSQL

---
+++ modules/powershell/powerupsql.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for PowerUpSQL
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Shaun Webber (Shaunography)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update PowerUpSQL - the powershell SQL Server discovery, weak configuration auditing, privilege escalation on scale, and post exploitation actions such as OS command execution"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/NetSPI/PowerUpSQL"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="poweropsql"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
