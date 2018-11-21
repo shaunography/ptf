c6cad78818caf609c850ea131b46ae4f3c3ce701
Shaun Webber <shaun.webber@sec-1.com>
2018-11-21 14:50:51 +0000

    added DomainPasswordSpray

---
+++ modules/powershell/domainpasswordspray.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for DomainPasswordSpray
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Shaun Webber (Shaunography)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update DomainPasswordSpray - a tool written in PowerShell to perform a password spray attack against users of a domain"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/dafthack/DomainPasswordSpray"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="domainpasswordspray"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
