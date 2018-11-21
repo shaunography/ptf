4af953f47cf907619e86fe16e5732812192d5acb
Shaun Webber <shaun.webber@sec-1.com>
2018-11-21 15:40:00 +0000

    added inveigh

---
+++ modules/powershell/inveigh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Inveigh
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Shaun Webber (Shaunography)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Inveigh - a PowerShell ADIDNS/LLMNR/mDNS/NBNS spoofer and man-in-the-middle tool"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/Kevin-Robertson/Inveigh.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="inveigh"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
